3fe8b2a32873c4d226b4b8859e5ba6fe7f558fe6
Zane Rockenbaugh <zane@liquid-labs.com>
2021-07-01 11:51:27 -0500

    relocate option processing for sharing; tweak names

--- src/google/lib/common-options-processing.sh
+++ src/google/lib/common-options-processing.sh
@@ -1,11 +1,18 @@
 # These are common helper functions meant to be called by higher level functions utilizing the common options. They rely
 # on external varaiables.
 
-google-lib-common-options-spec() {
+google-lib-common-core-options-spec() {
   echo 'PROJECT_ID= NON_INTERACTIVE: NO_ACCOUNT_REPORT: SKIP_AUTH_CHECK:'
 }
 
-google-lib-common-options-processing() {
+google-lib-common-options-check-access-and-report() {
+  [[ -n "${SKIP_AUTH_CHECK:-}" ]] || {
+    google-check-access
+    [[ -n "${NO_ACCOUNT_REPORT:-}" ]] || google-account-report
+  }
+}
+
+google-lib-common-core-options-processing() {
   if [[ -z "${PROJECT_ID:-}" ]]; then
     # TODO: allow project set from active config...
     if [[ -n "${NON_INTERACTIVE:-}" ]]; then
@@ -18,9 +25,18 @@ google-lib-common-options-processing() {
   google-lib-common-options-check-access-and-report
 }
 
-google-lib-common-options-check-access-and-report() {
-  [[ -n "${SKIP_AUTH_CHECK:-}" ]] || {
-    google-check-access
-    [[ -n "${NO_ACCOUNT_REPORT:-}" ]] || google-account-report
-  }
+google-lib-common-org-options-spec() {
+  echo 'ORGANIZATION_ID='
+}
+
+google-lib-common-org-options-processing() {
+  [[ -n "${ORGANIZATION_ID}" ]] || echoerrandexit 'Organization ID (--organization-id=xxxx) is required.'
+}
+
+google-lib-common-create-options-spec() {
+  echo 'CREATE_IF_NECESSARY NO_RETRY_NAMES: RETRY_COUNT:= ID_OUTPUT_VAR'
+}
+
+google-lib-common-create-options-processing() {
+  [[ -n "${RETRY_COUNT}" ]] || RETRY_COUNT=3
 }
