9310a22f3dac0bfd567d2830c84d00b97b69a9df
Zane Rockenbaugh <zane@liquid-labs.com>
2021-08-30 21:13:37 -0500

    auto lint fixes

--- src/appengine/app.js
+++ src/appengine/app.js
@@ -25,7 +25,7 @@ import { Storage } from '@google-cloud/storage'
 
 import { setupAccessLib } from './lib/access'
 
-'use strict';
+'use strict'
 
 // Get basic project info
 const projectId = process.env.GOOGLE_CLOUD_PROJECT
@@ -75,78 +75,78 @@ const htmlOpen = (path) => `<!doctype html>
 
 const htmlEnd = () => `
 </body>
-</html>`;
+</html>`
 
 const markedOptions = {
-  smartLists: true
+  smartLists : true
 }
 
-const readBucketFile = async ({ path, res }) => {
+const readBucketFile = async({ path, res }) => {
   try {
     // first, check if file exists.
-    const file = bucket.file(path);
-    const [ exists ] = await file.exists();
+    const file = bucket.file(path)
+    const [exists] = await file.exists()
     if (exists) {
-      const reader = file.createReadStream();
+      const reader = file.createReadStream()
       reader.on('error', (err) => {
-        console.error(`Error while reading file: ${err}`);
-        res.status(500).send(`Error reading file '${path}': ${err}`).end();
+        console.error(`Error while reading file: ${err}`)
+        res.status(500).send(`Error reading file '${path}': ${err}`).end()
       })
 
       // is the file an image type?
       const imageMatchResults = path.match(commonImageFiles)
       if (path.match(commonImageFiles)) {
-        res.writeHead(200,{'content-type':`image/${imageMatchResults[1].toLowerCase()}`});
+        res.writeHead(200, { 'content-type' : `image/${imageMatchResults[1].toLowerCase()}` })
       }
 
       if (path.endsWith('.md')) {
-        let markdown = '';
-        reader.on('data', (d) => { markdown += d; })
+        let markdown = ''
+        reader.on('data', (d) => { markdown += d })
           .on('end', () => {
-            const breadcrumbs = renderBreadcrumbs(path);
-            res.send(`${htmlOpen(path)}\n\n${breadcrumbs}\n\n${marked(markdown, markedOptions)}\n\n${breadcrumbs}\n\n${htmlEnd()}`);
-          });
+            const breadcrumbs = renderBreadcrumbs(path)
+            res.send(`${htmlOpen(path)}\n\n${breadcrumbs}\n\n${marked(markdown, markedOptions)}\n\n${breadcrumbs}\n\n${htmlEnd()}`)
+          })
       }
       else {
-        reader.pipe(res);
+        reader.pipe(res)
       }
     }
     else { // No such file, send 404
-      res.status(404).send(`No such file: '${path}'`).end();
+      res.status(404).send(`No such file: '${path}'`).end()
     }
   }
   catch (err) {
-    console.error(`Caught exception while processing '${path}'.`);
-    req.status(500).send(`Error reading: ${path}`);
-    return next(err); // for express error handling
+    console.error(`Caught exception while processing '${path}'.`)
+    req.status(500).send(`Error reading: ${path}`)
+    return next(err) // for express error handling
   }
 }
 
-const startSlash = /^\//;
-const endSlash = /\/$/;
+const startSlash = /^\//
+const endSlash = /\/$/
 
 const renderBreadcrumbs = (path, options) => {
-  let output = "";
-  if (!path || path === '') { return output; }
+  let output = ''
+  if (!path || path === '') { return output }
 
-  const { format='html' } = options || {};
+  const { format = 'html' } = options || {}
 
   // We remove the end slash to avoid an empty array element.
-  const pathBits = path.replace(endSlash, '').split('/');
+  const pathBits = path.replace(endSlash, '').split('/')
   // Each path bit represents a step back, but we step back into the prior element. E.g., if we see path "foo/bar",
   // so stepping back one takes us to foo and stepping back two takes us to the root. So we unshift a root element and
   // pop the last element to make everything match up.
-  pathBits.unshift('&lt;root&gt;');
-  pathBits.pop();
-  const pathBitsLength = pathBits.length;
+  pathBits.unshift('&lt;root&gt;')
+  pathBits.pop()
+  const pathBitsLength = pathBits.length
   // Breadcrumbs for a file end with the current dir and then move back. For a directory, you're stepping back in each
   // iteration.
   const linkBits = path.match(fileRegex)
     ? pathBits.map((b, i) => (i + 1) === pathBitsLength
-        ? '.'
-        : Array(pathBitsLength - (i + 1)).fill('..').join('/')
-      )
-    : pathBits.map((b, i) => Array(pathBitsLength - i).fill('..').join('/'));
+      ? '.'
+      : Array(pathBitsLength - (i + 1)).fill('..').join('/')
+    )
+    : pathBits.map((b, i) => Array(pathBitsLength - i).fill('..').join('/'))
 
   for (let i = 0; i < pathBits.length; i += 1) {
     if (format === 'markdown') {
@@ -157,73 +157,73 @@ const renderBreadcrumbs = (path, options) => {
     }
   }
 
-  return output;
+  return output
 }
 
 const renderFiles = ({ path, files, folders, res }) => {
   // Our 'path' comes in full relative from the root. However, we want to show only the relative bits.
-  const deprefixer = new RegExp(`${path}/?`);
+  const deprefixer = new RegExp(`${path}/?`)
   // open up with some boilerplace HTML
-  let html =`${htmlOpen(path)}
+  let html = `${htmlOpen(path)}
   <div id="breadcrumbs">
     ${renderBreadcrumbs(path)}
   </div>
-  <h1>${path}</h1>`;
+  <h1>${path}</h1>`
 
   if (folders.length > 0) {
     html += `
   <h2 id="folders">Folders</h2>
     ${folders.length} total
-  <ul>\n`;
+  <ul>\n`
     folders.forEach(folder => {
-      const localRef = folder.replace(deprefixer, '');
-      html += `    <li><a href="${encodeURIComponent(localRef.replace(endSlash, ''))}/">${localRef}</a></li>\n`;
-    });
+      const localRef = folder.replace(deprefixer, '')
+      html += `    <li><a href="${encodeURIComponent(localRef.replace(endSlash, ''))}/">${localRef}</a></li>\n`
+    })
 
-    html += `  </ul>`;
+    html += '  </ul>'
   }
 
   if (files && files.length > 0) {
     html += `
   <h2 id="files">Files</h2>
   ${files.length} total
-  <ul>\n`;
+  <ul>\n`
 
     files.forEach(file => {
-      const localRef = file.name.replace(deprefixer, '');
-      html += `    <li><a href="${encodeURIComponent(localRef)}">${localRef}</a></li>\n`;
-    });
+      const localRef = file.name.replace(deprefixer, '')
+      html += `    <li><a href="${encodeURIComponent(localRef)}">${localRef}</a></li>\n`
+    })
 
-    html += `  </ul>`;
+    html += '  </ul>'
   }
-  html += htmlEnd();
+  html += htmlEnd()
 
-  res.send(html).end();
+  res.send(html).end()
 }
 
 const indexerQueryOptions = {
-  delimiter: '/',
-  includeTrailingDelimiter: true,
-  autoPaginate: false // ?? necessary to see sub-folders
+  delimiter                : '/',
+  includeTrailingDelimiter : true,
+  autoPaginate             : false // ?? necessary to see sub-folders
 }
 
-const indexBucket = async ({ path, res }) => {
+const indexBucket = async({ path, res }) => {
   try { // TODO: I think now that we asyncHandler, we can forgo generic try-catch blocks
     // We expect the root path to be ''; all others should end with a '/'
     if (path !== '' && !path.match(endSlash)) {
-      res.redirect(301, `${path}/`).end();
+      res.redirect(301, `${path}/`).end()
     }
 
-    const indexPath = `${path}index.html`;
-    console.log(`indexPath is: ${indexPath}`);
-    const file = bucket.file(indexPath);
-    const [ exists ] = await file.exists();
+    const indexPath = `${path}index.html`
+    console.log(`indexPath is: ${indexPath}`)
+    const file = bucket.file(indexPath)
+    const [exists] = await file.exists()
     if (exists) {
-      return readBucketFile({ path: indexPath, res });
+      return readBucketFile({ path : indexPath, res })
     }
 
-    let folders = [];
-    const query = Object.assign({ prefix: path }, indexerQueryOptions)
+    let folders = []
+    const query = Object.assign({ prefix : path }, indexerQueryOptions)
 
     // OK, the Cloud Storage API (as of v5) is finicky and will only show you files in the 'await' version, e.g.:
     //
@@ -237,33 +237,33 @@ const indexBucket = async ({ path, res }) => {
     // prefix. These can be mapped to logical sub-folders in our bucket scheme.
     const indexPager = (err, files, nextQuery, apiResponse) => {
       if (err) {
-        res.setStatus(500).send(`Error while processing results: ${err}`).end();
+        res.setStatus(500).send(`Error while processing results: ${err}`).end()
       }
       // all good!
       if (apiResponse.prefixes && apiResponse.prefixes.length > 0) {
-        folders = folders.concat(apiResponse.prefixes);
+        folders = folders.concat(apiResponse.prefixes)
       }
 
       if (nextQuery) {
-        bucket.getFiles(nextQuery, indexPager);
+        bucket.getFiles(nextQuery, indexPager)
       }
       else { // we've built up all the folders
         // If there's nothing here, treat as 404
         if ((!files || files.length === 0) && folders.length === 0) {
-          res.status(404).send(`No such folder: '${path}'`).end();
+          res.status(404).send(`No such folder: '${path}'`).end()
         }
         else {
-          renderFiles({ path, files, folders, res });
+          renderFiles({ path, files, folders, res })
         }
       }
     }
 
     // here's where we actually kick everything off by doing the search.
-    bucket.getFiles(query, indexPager);
+    bucket.getFiles(query, indexPager)
   } // try
   catch (e) {
-    res.status(500).send(`Explosion! ${e}`);
-    return next(e); // for express error handling
+    res.status(500).send(`Explosion! ${e}`)
+    return next(e) // for express error handling
   }
 }
 
@@ -272,7 +272,7 @@ const indexBucket = async ({ path, res }) => {
 // request processing setup
 
 // async because our handsers are async
-const commonProcessor = (render) => async (req, res) => {
+const commonProcessor = (render) => async(req, res) => {
   let userEmail = null
   try {
     const ticket = await accessLib.verifyToken(req)
@@ -280,36 +280,36 @@ const commonProcessor = (render) => async (req, res) => {
     // console.log(`Requesting user: ${userEmail}`)
   }
   catch (e) {
-    console.error(`Exception while verifying access: ${e}`);
-    res.status(401).send(`Request authorization token could not be verified.\n${e}`);
-    return next(e);
+    console.error(`Exception while verifying access: ${e}`)
+    res.status(401).send(`Request authorization token could not be verified.\n${e}`)
+    return next(e)
   }
 
   // Cloud storage doesn't like an initial '/', so we remove any.
-  const path = decodeURIComponent(req.path.replace(startSlash, ''));
+  const path = decodeURIComponent(req.path.replace(startSlash, ''))
 
   res.on('error', (err) => {
-    console.error(`Error in the response output stream: ${err}`);
+    console.error(`Error in the response output stream: ${err}`)
   })
 
   try {
-    render({ path, res });
+    render({ path, res })
   }
   catch (e) {
-    console.error(`Exception while rendering: ${e}`);
-    res.status(500).send(`Exception encountered while rendering result: ${e}`);
-    return next(e);
+    console.error(`Exception while rendering: ${e}`)
+    res.status(500).send(`Exception encountered while rendering result: ${e}`)
+    return next(e)
   }
 }
 
-app.get(fileRegex, asyncHandler(commonProcessor(readBucketFile)));
+app.get(fileRegex, asyncHandler(commonProcessor(readBucketFile)))
 // if it's not a file, maybe it's a bucket.
-app.get('*', asyncHandler(commonProcessor(indexBucket)));
+app.get('*', asyncHandler(commonProcessor(indexBucket)))
 
 // start the server
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
-});
+  console.log(`App listening on port ${PORT}`)
+  console.log('Press Ctrl+C to quit.')
+})
 
 export default app
--- src/appengine/lib/access.js
+++ src/appengine/lib/access.js
@@ -1,29 +1,29 @@
 import { OAuth2Client } from 'google-auth-library'
 
 const setupAccessLib = ({ projectId, projectNumber }) => {
-
-  let expectedAudience = null;
+  let expectedAudience = null
   if (projectNumber && projectId) {
     // Expected Audience for App Engine.
-    expectedAudience = `/projects/${projectNumber}/apps/${projectId}`;
-  } else if (projectNumber && backendServiceId) { // for future ref; not used here
+    expectedAudience = `/projects/${projectNumber}/apps/${projectId}`
+  }
+  else if (projectNumber && backendServiceId) { // for future ref; not used here
     // Expected Audience for Compute Engine
-    expectedAudience = `/projects/${projectNumber}/global/backendServices/${backendServiceId}`;
+    expectedAudience = `/projects/${projectNumber}/global/backendServices/${backendServiceId}`
   }
 
   return {
-    verifyToken: async (req) => {
+    verifyToken : async(req) => {
       const iapJwt = req.headers?.['x-goog-iap-jwt-assertion']
 
-      const oAuth2Client = new OAuth2Client();
+      const oAuth2Client = new OAuth2Client()
       // Verify the id_token, and access the claims.
-      const response = await oAuth2Client.getIapPublicKeys();
+      const response = await oAuth2Client.getIapPublicKeys()
       const ticket = await oAuth2Client.verifySignedJwtWithCertsAsync(
         iapJwt,
         response.pubkeys,
         expectedAudience,
         ['https://cloud.google.com/iap']
-      );
+      )
 
       return ticket
     }
